b4fb42809a5e5c0a42917827166f75b6a0573b0f
XiledCreator <xiledcreator@gmail.com>
2020-10-20 20:59:09 -0400

    Listener On DOM

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 function getNewName() {
   chrome.tabs.executeScript({
     file: 'getNewName.js'
